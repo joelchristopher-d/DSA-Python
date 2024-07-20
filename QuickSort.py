d6cc35cdf56431ad27d15b64a416a5761ba7465c
Joel Christopher <118204275+joelchristopher-d@users.noreply.github.com>
2024-07-20 23:31:35 +0530

    Update QuickSort.py

--- QuickSort.py
+++ QuickSort.py
@@ -1,3 +1,5 @@
+# high as pivot point
+
 def Partition(arr,low,high):
     pivot = arr[high]
     i=low-1
@@ -14,6 +16,30 @@ def QuickSort(arr,low,high):
         QuickSort(arr,low,pi-1)
         QuickSort(arr,pi+1,high)
         
+arr=[5,7,6,9,4,8,1,2,3,11,10]
+low=0
+high=len(arr)-1
+QuickSort(arr,low,high)
+print(arr)
+
+# low as Pivot point
+
+def Partition(arr,low,high):
+    pivot = arr[low]
+    i=high+1
+    for j in range(high,low,-1):
+        if arr[j]>=pivot:
+            i-=1
+            arr[i],arr[j]=arr[j],arr[i]
+    arr[i-1],arr[low]=arr[low],arr[i-1]
+    return i-1
+
+def QuickSort(arr,low,high):
+    if low<high:
+        pi = Partition(arr,low,high)
+        QuickSort(arr,low,pi-1)
+        QuickSort(arr,pi+1,high)
+
 arr=[5,7,6,9,4,8,1,2,3,11,10]
 low=0
 high=len(arr)-1
